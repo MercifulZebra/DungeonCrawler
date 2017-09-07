d417b0881897af74fc17701b53001b54463327d4
MercifulZebra <Zwcondon@vt.edu>
2017-09-07 08:27:26 -0400

    Begin addition of loading tileset and added default tile images

--- trunk/DungeonCrawlerUI/tileset.cpp
+++ trunk/DungeonCrawlerUI/tileset.cpp
@@ -1,6 +1,48 @@
 #include "tileset.h"
 
-TileSet::TileSet(QObject *parent) : QObject(parent)
+#include "logger.h"
+
+#include <QFile>
+//"resources/default/index.txt"
+TileSet::TileSet(logger::Logger *nLog, QString defaultIndex_path, QObject *parent) : QObject(parent),
+    log(nLog),
+    index_path(defaultIndex_path),
+    default_path(defaultIndex_path),
+    wall_filenames("wall.png"),
+    insideCorner_filenames("inside_corner.png"),
+    outsideCorner_filenames("outside_corner.png"),
+    island_filenames("island.png"),
+    fill_filenames("fill.png"),
+    floor_filenames("floor.png"),
+    peninsula_filenames("peninsula.png"),
+    wall_pixmap(),
+    insideCorner_pixmap(),
+    outsideCorner_pixmap(),
+    island_pixmap(),
+    fill_pixmap(),
+    floor_pixmap(),
+    peninsula_pixmap()
+
 {
 
+}
+
+bool TileSet::loadTileSet(QString nIndex) {
+
+    if (log == NULL) {
+        return false;
+    }
+
+    QFile indexFile(index_path);
+    bool loadSuccess_flag = true;
+
+    if (!indexFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+
+    }
+    else {
+        log->err(QString("Unable to open index file \"%1\"").arg(index_path));
+        loadSuccess_flag = false;
+    }
+
+    return loadSuccess_flag;
 }
--- trunk/DungeonCrawlerUI/tileset.h
+++ trunk/DungeonCrawlerUI/tileset.h
@@ -2,12 +2,43 @@
 #define TILESET_H
 
 #include <QObject>
+#include <QPixmap>
+#include <QStringList>
+#include <QVector>
+
+namespace logger {
+class Logger;
+}
 
 class TileSet : public QObject
 {
     Q_OBJECT
 public:
-    explicit TileSet(QObject *parent = 0);
+    explicit TileSet(logger::Logger *nLog, QString defaultIndex_path, QObject *parent = 0);
+
+    bool loadTileSet(QString nIndex);
+
+private:
+    logger::Logger *log;
+
+    QString index_path;
+    QString default_path;
+
+    QStringList wall_filenames;
+    QStringList insideCorner_filenames;
+    QStringList outsideCorner_filenames;
+    QStringList island_filenames;
+    QStringList fill_filenames;
+    QStringList floor_filenames;
+    QStringList peninsula_filenames;
+
+    QVector<QPixmap> wall_pixmap;
+    QVector<QPixmap> insideCorner_pixmap;
+    QVector<QPixmap> outsideCorner_pixmap;
+    QVector<QPixmap> island_pixmap;
+    QVector<QPixmap> fill_pixmap;
+    QVector<QPixmap> floor_pixmap;
+    QVector<QPixmap> peninsula_pixmap;
 
 signals:
 
